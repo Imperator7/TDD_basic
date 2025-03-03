c61722e085295ac167a3a6253b5f64eac9299975
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-03-03 11:44:50 +0700

    feat: testcase that raise NotEnoughMoney when having less money than the drink's price

--- logic.py
+++ logic.py
@@ -6,14 +6,21 @@ class Cafe:
 
   def buy(self, drink: str, paid_money: float):
     self._validate_drink(drink)
-
+    self._validate_amount_of_money(drink, paid_money)
 
     change = paid_money - self.PRICE[drink]
     return drink, change
   
+  def _validate_amount_of_money(self, drink, paid_money):
+    if self.PRICE[drink] > paid_money:
+      raise NotEnoughMoney()
+
   def _validate_drink(self, drink):
     if not drink in self.PRICE:
       raise DrinkNotInMenu()
     
 class DrinkNotInMenu(Exception):
+  pass
+
+class NotEnoughMoney(Exception):
   pass
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,5 +1,5 @@
 from pytest import raises
-from logic import Cafe, DrinkNotInMenu
+from logic import Cafe, DrinkNotInMenu, NotEnoughMoney
 
 def test_buy_tea_with_50_baht_change_should_be_30_baht():
   cafe = Cafe()
@@ -11,7 +11,12 @@ def test_should_return_coffee_and_the_change_of_70_baht_when_buying_coffee_with_
   drink, change = cafe.buy("coffee", 100.0)
   assert (drink, change) == ("coffee", 70.0)
 
-def test_shoud_return_raise_DrinkNotInMenu_when_buying_cocoa():
+def test_shoud_raise_DrinkNotInMenu_when_buying_cocoa():
   cafe = Cafe()
   with raises(DrinkNotInMenu):
     cafe.buy("cocoa", 50.0)
+
+def test_should_raise_NotEnoughMoney_when_buying_coffee_with_1_baht():
+  cafe = Cafe()
+  with raises(NotEnoughMoney):
+    cafe.buy("coffee", 1.0)
